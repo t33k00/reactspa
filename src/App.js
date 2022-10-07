659a135eff1f8d9e6e572b7da78a17b9bb70edaa
t33k00 <tmkyllonen@gmail.com>
2022-10-07 13:26:43 +0300

    yeesyees

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './sites/Home';
+import About from './sites/About';
+import Header from './sites/Header';
+import NotFound from './sites/NotFound';
+import ContactUs from './sites/ContactUs';
+import Footer from './sites/Footer';
+import Navbar from './sites/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contactus" element={<ContactUs/>}/>
+      <Route path="/*" element={<NotFound/>}/>
+    </Routes>
+    <Footer/>
+    </>
   );
 }
 
